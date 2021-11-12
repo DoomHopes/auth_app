8fc7e90f8ea4e5972eaa03a05601f79004981ced
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-11-12 09:56:41 +0200

    some change

--- lib/domain/auth/email.dart
+++ lib/domain/auth/email.dart
@@ -1,4 +1,40 @@
-class Email {
+class EmailAddress {
   final String value;
-  const Email(this.value);
+
+  factory EmailAddress(String input) {
+    assert(input != null);
+    return EmailAddress._(
+      validateEmail(input),
+    );
+  }
+
+  const EmailAddress._(this.value);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is EmailAddress && other.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => 'Email(value: $value)';
+}
+
+String validateEmail(String input) {
+  const emailRegex =
+      r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+  if (RegExp(emailRegex).hasMatch(input)) {
+    return input;
+  } else {
+    throw InvalidEmailException(failedValue: input);
+  }
+}
+
+class InvalidEmailException implements Exception {
+  final String failedValue;
+  InvalidEmailException({required this.failedValue});
 }
