f8a78bf08190b2775c6f5108b72111e903f288da
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-11-12 14:29:22 +0200

    add background

--- lib/presentation/first_page.dart
+++ lib/presentation/first_page.dart
@@ -6,8 +6,17 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Bite my shiny metal ass'),
+      body: Stack(
+        children: <Widget>[
+          SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: Image.asset(
+              'lib/presentation/images/background_image.jpg',
+              fit: BoxFit.fill,
+            ),
+          ),
+        ],
       ),
     );
   }
