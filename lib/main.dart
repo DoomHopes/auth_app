d6c416548bc3985e2222206720681a66958e6674
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-11-12 14:18:44 +0200

    add first page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:auth_app/presentation/first_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,9 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(
-        body: Center(child: Text('Bite my shiny metal ass')),
-      ),
+      home: FirstPage(),
     );
   }
 }
