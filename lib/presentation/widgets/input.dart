5827c96fd0e64bed6f88105c3cd06997b97a5f25
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-11-15 10:16:57 +0200

    add input widget

---
+++ lib/presentation/widgets/input.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+Widget input(Icon icon, String hint,
+    TextEditingController textEditingController, bool obscure) {
+  return Container(
+    padding: const EdgeInsets.only(left: 20, right: 20),
+    child: TextField(
+      controller: textEditingController,
+      obscureText: obscure,
+      style: const TextStyle(fontSize: 20),
+      decoration: InputDecoration(
+        hintStyle: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+          color: Colors.white30,
+        ),
+        hintText: hint,
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white, width: 3),
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white54, width: 1),
+        ),
+        prefixIcon: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: IconTheme(
+            data: const IconThemeData(color: Colors.white),
+            child: icon,
+          ),
+        ),
+      ),
+    ),
+  );
+}
