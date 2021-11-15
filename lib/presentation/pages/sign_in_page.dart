06215b7c6a06dee1996fff264b2a4dfa782ae46e
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-11-15 10:29:17 +0200

    some change

--- lib/presentation/pages/sign_in_page.dart
+++ lib/presentation/pages/sign_in_page.dart
@@ -1,13 +1,48 @@
+import 'package:auth_app/presentation/widgets/button.dart';
+import 'package:auth_app/presentation/widgets/input.dart';
 import 'package:flutter/material.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Sign In Page'),
+      appBar: AppBar(
+        title: const Text('Sing In'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          const Icon(
+            Icons.login_sharp,
+            size: 150,
+          ),
+          Column(
+            children: <Widget>[
+              input(const Icon(Icons.login), 'Enter email', _emailController,
+                  false),
+              const SizedBox(
+                height: 10,
+              ),
+              input(const Icon(Icons.password), 'Enter password',
+                  _passwordController, true),
+            ],
+          ),
+          ButtonWidget(
+            text: 'Sign in',
+            onClicked: () {},
+          ),
+        ],
       ),
     );
   }
--- lib/presentation/pages/sing_up_page.dart
+++ lib/presentation/pages/sing_up_page.dart
@@ -1,13 +1,47 @@
+import 'package:auth_app/presentation/widgets/button.dart';
+import 'package:auth_app/presentation/widgets/input.dart';
 import 'package:flutter/material.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
 
   @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Sign Up Page'),
+      appBar: AppBar(
+        title: const Text('Sing Up'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          const Icon(
+            Icons.app_registration_outlined,
+            size: 150,
+          ),
+          Column(
+            children: <Widget>[
+              input(const Icon(Icons.login), 'Enter email', _emailController,
+                  false),
+              const SizedBox(
+                height: 10,
+              ),
+              input(const Icon(Icons.password), 'Enter password',
+                  _passwordController, true),
+            ],
+          ),
+          ButtonWidget(
+            text: 'Register',
+            onClicked: () {},
+          ),
+        ],
       ),
     );
   }
