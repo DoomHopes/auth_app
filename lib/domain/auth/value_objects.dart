dabcb1fcc4352f65c2ab933e8438de5a11158946
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-11-30 09:39:16 +0200

    little refactor

--- lib/domain/auth/value_objects.dart
+++ lib/domain/auth/value_objects.dart
@@ -32,17 +32,3 @@ class Password extends ValueObject<String> {
 
   const Password._(this.value);
 }
-
-/**
- * void showingTheEmailAddressOrFailure(EmailAddress emailAddress) {
-  // Longer to write but we can get the failure instance
-  final emailText1 = emailAddress.value.fold(
-    (left) => 'Failure happened, more precisely: $left',
-    (right) => right,
-  );
-
-  // Shorter to write but we cannot get the failure instance
-  final emailText2 =
-      emailAddress.value.getOrElse(() => 'Some failure happened');
-}
- */
