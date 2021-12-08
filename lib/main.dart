38ae1eb195f87c0837564f0238161071f8e25236
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-12-08 11:35:21 +0200

    some change

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
-import 'package:auth_app/presentation/pages/first_page.dart';
 import 'package:flutter/material.dart';
+
+import 'presentation/core/app_widget.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: const FirstPage(),
-    );
-  }
 }
---
+++ lib/presentation/core/app_widget.dart
@@ -0,0 +1,15 @@
+import 'package:auth_app/presentation/pages/first_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: const FirstPage(),
+    );
+  }
+}
